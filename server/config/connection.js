06f20762fac220ef3898cdbec6edd8c0c8907626
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-11-14 23:21:21 +1030

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tajdinov:Taj3112143@cluster0.myt9aui.mongodb.net/record-store?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tajdinov:Taj3112143@cluster0.myt9aui.mongodb.net/recordStore?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
